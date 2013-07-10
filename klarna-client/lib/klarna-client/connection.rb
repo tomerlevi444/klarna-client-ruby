ad52e7bb4fa176c6d0bec958fa525961f51ed0a9
Tomer Alon <tomer.alon@klarna.com>
2013-07-10 09:46:08 +0300

    Add first basic connection initializer spec

--- klarna-client/lib/klarna-client/connection.rb
+++ klarna-client/lib/klarna-client/connection.rb
@@ -1,6 +1,13 @@
 module Klarna
 	module Client
 		class Connection
+    		attr_accessor :xmlrpc_hostname
+    		attr_accessor :xmlrpc_port
+
+    		def initialize(xmlrpc_hostname, xmlrpc_port)
+    			self.xmlrpc_hostname = xmlrpc_hostname
+    			self.xmlrpc_port     = xmlrpc_port
+    		end
 		end
 	end
 end
--- klarna-client/spec/lib/klarna-client/connection_spec.rb
+++ klarna-client/spec/lib/klarna-client/connection_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Klarna::Client::Connection do
+	describe '#initialize' do
+		context 'given hostname and port' do
+    		attr_accessor :xmlrpc_hostname
+    		attr_accessor :xmlrpc_port
 
+			let(:xmlrpc_hostname) { 'klarna_host' }
+			let(:xmlrpc_port)     { 12345 }
+			let(:connection)      { Klarna::Client::Connection.new(xmlrpc_hostname, xmlrpc_port) }
+
+			it 'initializes the hostname attribute' do
+				expect(connection.xmlrpc_hostname).to eq(xmlrpc_hostname)
+			end
+
+			it 'initializes the port attribute' do
+				expect(connection.xmlrpc_port).to eq(xmlrpc_port)
+			end
+		end
+	end
+
+	describe '#get_addresses' do
+	end
 end
