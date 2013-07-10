52279e32b8cb16d1cfa6a306c350ecf2fe095147
Tomer Alon <tomer.alon@klarna.com>
2013-07-10 16:32:20 +0300

    Some hacking to make the getAddress call work

--- klarna-client/lib/klarna-client.rb
+++ klarna-client/lib/klarna-client.rb
@@ -1,7 +1,27 @@
-require "klarna-client/version"
+require 'digest'
+require 'klarna-client/version'
+require 'klarna-client/connection'
 
 module Klarna
-  module Client
-    # Your code goes here...
-  end
+	module Client
+		class Xxx
+			def initialize(hostname, port, store_id, store_secret)
+				@connection      = Klarna::Client::Connection.new(hostname, port)
+				@store_id        = store_id
+				@store_secret    = store_secret
+			end
+
+			def get_addresses(pno, pno_encoding, type, client_ip)
+				@connection.xmlrpc_call('get_addresses', '4.1', 'ruby_client_SLOT', pno, @store_id, secret(pno), pno_encoding, type, client_ip)
+			end
+
+			private
+
+			def secret(pno)
+				message = [@store_id, pno, @store_secret].join(':')
+
+				Digest::SHA512.base64digest(message)
+			end
+		end
+	end
 end
