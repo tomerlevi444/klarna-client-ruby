5cc07cbd1a4b8ddf155e270412805f2b96f1682f
Daniel Salmeron Amselem <daniel.amselem@gmail.com>
2013-07-18 11:29:32 +0300

    Filter sensitive data from VCR fixtures.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,10 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
+
+  c.filter_sensitive_data('<KlARNA_URL>')  { ENV['KLARNA_URL'] }
+  c.filter_sensitive_data('<KlARNA_PORT>') { ENV['KLARNA_PORT'] }
+
   c.configure_rspec_metadata!
 end
 
