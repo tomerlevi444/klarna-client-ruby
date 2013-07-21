918c8bf12253a84ff2628af4f2ca6de7a8ae0d9d
Hagai Levin <hagai.levin@klarna.com>
2013-07-21 03:12:11 +0300

    Add specs for client support for 'activate' and spec for 'reserve amount' class method

--- spec/lib/klarna/client_spec.rb
+++ spec/lib/klarna/client_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe Klarna::Client do
@@ -25,7 +27,7 @@ describe Klarna::Client do
           '12345',
           'Ankeborg',
           '209'
-      ],
+        ],
         [
           'Testcompany-se',
           'lillegatan 1',
@@ -133,29 +135,17 @@ describe Klarna::Client do
     let(:params) do
       {
         :amount           => 199,
-        :bank_info        => [],
         :billing_address  => person_address,
-        :client_ip        => '',
-        :comment          => '',
         :country          => 209,
         :currency         => 0,
         :delivery_address => person_address,
-        :extra_info       => [],
         :flags            => 0,
-        :gender           => '',
         :goods_list       => goods_list,
-        :income_expense   => [],
         :language         => 138,
-        :order_id_1       => '',
-        :order_id_2       => '',
         :pclass           => -1,
-        :pno              => pno,
+        :pno              => '410321-9202',
         :pno_encoding     => 2,
-        :reference        => '',
-        :reference_code   => '',
-        :session_id       => {},
         :shipment_info    => {:delay_adjust => 1},
-        :travel_info      => []
       }
     end
 
@@ -163,8 +153,6 @@ describe Klarna::Client do
       let(:client) { build(:client) }
 
       context 'given a person with PNO 410321-9202' do
-        let(:pno) { '410321-9202' }
-
         before do
           VCR.use_cassette 'reserve_amount for PNO 410321-9202' do
             @reservation = client.reserve_amount(params)
@@ -183,10 +171,103 @@ describe Klarna::Client do
 
         describe 'second element of the response' do
           it 'is an integer representing the reservation status' do
-            expect(@reservation.last).to be_an(Integer)
+            expect(@reservation.last).to eq(1)
+          end
+        end
+      end
+    end
+
+    describe 'class method' do
+      before do
+        setup_configuration
+      end
+
+      context 'given a person with PNO 410321-9202' do
+        before do
+          VCR.use_cassette 'reserve_amount for PNO 410321-9202' do
+            @reservation = Klarna::Client.reserve_amount(params)
+          end
+        end
+
+        it 'returns an array' do
+          expect(@reservation).to be_an(Array)
+        end
+
+        describe 'first element of the response' do
+          it 'is a string representing the reservation number' do
+            expect(@reservation.first).to be_a(String)
+          end
+        end
+
+        describe 'second element of the response' do
+          it 'is an integer representing the reservation status' do
+            expect(@reservation.last).to eq(1)
           end
         end
       end
     end
   end
+
+  describe 'activate' do
+    let(:params) { {:rno => '60'} }
+
+    describe 'instance method' do
+      let(:client) { build(:client) }
+
+      context 'given a reservation with RNO 60' do
+        before do
+          VCR.use_cassette 'activate for RNO 60' do
+            @invoice = client.activate(params)
+          end
+        end
+
+        it 'returns an array' do
+          expect(@invoice).to be_an(Array)
+        end
+
+        describe 'first element of the response' do
+          it 'is a string representing the risk status' do
+            expect(@invoice.first).to eq("ok")
+          end
+        end
+
+        describe 'second element of the response' do
+          it 'is a string representing the invoice number' do
+            expect(@invoice.last).to be_a(String)
+          end
+        end
+      end
+    end
+
+    describe 'class method' do
+      before do
+        setup_configuration
+      end
+
+      context 'given a reservation with RNO 60' do
+        before do
+          VCR.use_cassette 'activate for RNO 60' do
+            @invoice = Klarna::Client.activate(params)
+          end
+        end
+
+        it 'returns an array' do
+          expect(@invoice).to be_an(Array)
+        end
+
+        describe 'first element of the response' do
+          it 'is a string representing the risk status' do
+            expect(@invoice.first).to eq("ok")
+          end
+        end
+
+        describe 'second element of the response' do
+          it 'is a string representing the invoice number' do
+            expect(@invoice.last).to be_a(String)
+          end
+        end
+      end
+    end
+  end
+
 end
