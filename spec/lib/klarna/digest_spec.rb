892f31cbdebbf2a9c17c45557f12734b86e37235
Hagai Levin <hagai.levin@klarna.com>
2013-07-21 16:11:33 +0300

    Fix context description

--- spec/lib/klarna/digest_spec.rb
+++ spec/lib/klarna/digest_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Klarna::Digest do
   describe '.for' do
-    context 'given an array of shit' do
+    context 'given an array with some parameters' do
       let(:array) { [1,'foo', :bar] }
 
-      it 'returns the right disgest' do
+      it 'returns the right digest' do
         expect(Klarna::Digest.for(array)).to eq('+mhT3NFF3HrvsBledhMRa98+H+1ES5JSVzL8YoWGr8LHosHgtqY2kJJ5fRHYI7AEKAig7EaanDv9ZGyHXTN8Kw==')
       end
     end
