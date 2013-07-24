8b05eeed038045ae6e2d93c38dae16e25c7fb88c
Hagai Levin <hagai.levin@klarna.com>
2013-07-24 10:57:09 +0300

    Add support for get_addresses with TNO

--- lib/klarna/methods/get_addresses.rb
+++ lib/klarna/methods/get_addresses.rb
@@ -4,15 +4,20 @@ module Klarna
   module Methods
     module GetAddresses
 
+      PNO_TYPE = 5
+      TNO_TYPE = 6
+
       def self.xmlrpc_name
         'get_addresses'
       end
 
       def self.xmlrpc_params(store_id, store_secret, api_version, client_name, params)
+        identifier = get_identifier(params)
+
         [
-          params[:pno],
+          identifier,
           store_id,
-          digest(store_id, params[:pno], store_secret),
+          digest(store_id, identifier, store_secret),
           params[:pno_encoding],
           params[:type],
           params.fetch(:client_ip, '')
@@ -21,12 +26,22 @@ module Klarna
 
       private
 
-      def self.digest(store_id, pno, store_secret)
-        array = [store_id, pno, store_secret]
+      def self.digest(store_id, identifier, store_secret)
+        array = [store_id, identifier, store_secret]
 
         Klarna::Digest.for(array)
       end
 
+      def self.get_identifier(params)
+        case params[:type]
+        when PNO_TYPE
+          params[:pno]
+        when TNO_TYPE
+          params[:tno]
+        else
+          raise ArgumentError, "Type is not valid. Got #{params[:type]}, expected #{PNO_TYPE} or #{TNO_TYPE}"
+        end
+      end
     end
   end
 end
